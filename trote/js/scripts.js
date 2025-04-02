61a998a770c4b9409506d02637588d57c89c56f5
igor <igorsouza1701@outlook.com>
2025-04-01 22:15:05 -0300

    dia 01

--- trote/js/scripts.js
+++ trote/js/scripts.js
@@ -1,19 +1,35 @@
  function calcular(){
     // vamos criar 2 variáveis
     // JS as variáveis não possuem tipo 
-    let mascote, homenagem, leite, kitAvulso, suplementoAvulso
+    let mascote, homenagem, leite, kit, suplemento, soma, equipe
     // recupera o valor do mascote digitado pelo usuario
-    mascote = document.getElementById("mascote").value 
+    mascote = Number (document.getElementById("mascote").value)
     // recupera o valor da homenagem digitado pelo usuario
-    homenagem = document.getElementById("homenagem").value
+    homenagem = Number(document.getElementById("homenagem").value)
     // recuperar a qtda de litros de leite
-    leite = document.getElementById("leite").value
+    leite = Number(document.getElementById("leite").value)
     // recuperar a qtde avulsa de kit alimento
-    kitAvulso = document.getElementById("kitAvulso").value
+    kit = Number(document.getElementById("kit").value)
    // recuperar a qtde avulsa de suplemento
-   suplementoAvulso = document.getElementById("suplementoAvulso").value
+   suplemento = Number(document.getElementById("suplemento").value)
     // calcular a soma
-    let soma = Number(mascote) + Number(homenagem) + (2 * Number(leite)) + (30 * Number(kitAvulso)) + (15 * Number(suplementoAvulso))
+    soma = (mascote) + (homenagem) + (2 * Number(leite))
+   // vamos calcular a pontuacao considerando metas de kit e suplemento
+    equipe= document.getElementById("equipe").value
+   if (equipe == "Laranja"){
+      if (kit >= 97) {
+         soma = soma + 5000 + ((kit-97) * 30)
+      }
+      else if (kit >= 78){
+         soma = soma + 4000 + ((kit-78) * 30)
+      }
+      else if (kit >= 49) {
+         soma = soma + 2500 + ((kit-49) * 30)
+      }
+      else if (kit >= 19){
+         soma = soma + 1000 + ((kit-19) * 30)
+      }
+   }
     // retorna o valor ao HTML
     // template string     
     document.getElementById("soma").innerHTML = `A soma é ${soma}`
