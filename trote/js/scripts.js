bec77e0b345529d5c2f299b777f8e76a2bc0fbf5
igor <igorsouza1701@outlook.com>
2025-04-03 20:22:43 -0300

    dia 03

--- trote/js/scripts.js
+++ trote/js/scripts.js
@@ -13,21 +13,25 @@
    // recuperar a qtde avulsa de suplemento
    suplemento = Number(document.getElementById("suplemento").value)
     // calcular a soma
-    soma = (mascote) + (homenagem) + (2 * Number(leite))
+    soma = Number(mascote) + Number(homenagem) + (2 *Number (leite))
    // vamos calcular a pontuacao considerando metas de kit e suplemento
     equipe= document.getElementById("equipe").value
    if (equipe == "Laranja"){
-      if (kit >= 97) {
-         soma = soma + 5000 + ((kit-97) * 30)
+      if (kit >= 97 && suplemento >=49) {
+         soma = soma + 5000 + ((kit-97) * 30) + ((suplemento - 49) * 15)
+
       }
-      else if (kit >= 78){
-         soma = soma + 4000 + ((kit-78) * 30)
+      else if (kit >= 78 && suplemento >= 39){
+         soma = soma + 4000 + ((kit-78) * 30) + ((suplemento - 39) * 15)
+
       }
-      else if (kit >= 49) {
-         soma = soma + 2500 + ((kit-49) * 30)
+      else if (kit >= 49 && suplemento >= 25) {
+        soma = soma + 2500 ((kit-49) * 30) + ((suplemento - 25) * 15)
+
       }
-      else if (kit >= 19){
-         soma = soma + 1000 + ((kit-19) * 30)
+      else if (kit >= 19 && suplemento >= 10){
+         soma = soma + 1000 + ((kit-19) * 30) + ((suplemento - 10) * 15)
+
       }
    }
     // retorna o valor ao HTML
